3035fd69fd4a20678f9f9ae2b39c2d0c78e3adfd
gulchak-d <gulchak@knu.ua>
2025-12-11 20:27:51 +0200

    Fix instance type to t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0a261c0e5f51090b1" # Amazon Linux 2023 (eu-central-1)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
